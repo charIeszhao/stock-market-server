6364e748ef0409d0114e554a939cf7b8420b343a
demonzoo <demonzoo712027@gmail.com>
2020-05-14 09:49:52 +0800

    update company service module
    Former-commit-id: 508825bc5d44edcc4c8950e5980ef4040230869f

---
+++ cloud-company-service/src/main/java/com/demo/stock/company/service/impl/CompanyServiceImpl.java
@@ -0,0 +1,40 @@
+package com.demo.stock.company.service.impl;
+
+import java.util.List;
+
+import com.demo.stock.entity.CompanyEntity;
+import com.demo.stock.company.service.CompanyService;
+
+public class CompanyServiceImpl implements CompanyService {
+
+	@Override
+	public List<CompanyEntity> listCompanies() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public CompanyEntity get(int companyId) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public CompanyEntity update(CompanyEntity company) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void delete(int companyId) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public CompanyEntity add(CompanyEntity company) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
