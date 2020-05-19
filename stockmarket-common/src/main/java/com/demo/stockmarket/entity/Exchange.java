82ee80ea6082f2aa44a5f002b9d93265d9f207f7
Charles Zhao <charles.zhao@cn.ibm.com>
2020-05-20 01:07:15 +0800

    format code
    Former-commit-id: 0c82d75b581d46d919b48334154c0a7016ebd3f1

--- stockmarket-common/src/main/java/com/demo/stockmarket/entity/Exchange.java
+++ stockmarket-common/src/main/java/com/demo/stockmarket/entity/Exchange.java
@@ -1,6 +1,9 @@
 package com.demo.stockmarket.entity;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
 import javax.persistence.Table;
 
 import lombok.Getter;
@@ -8,17 +11,23 @@ import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Entity
-@Table(name="t_exchange")
+@Table(name = "t_exchange")
 @NoArgsConstructor
 @Getter
 @Setter
 public class Exchange {
 
+	@Id
+	@GeneratedValue
+	@Column(name = "id")
 	private int id;
-	
+
+	@Column(name = "name", unique = true)
 	private String name;
-	
+
+	@Column(name = "description")
 	private String description;
-	
+
+	@Column(name = "address")
 	private String address;
 }
--- stockmarket-gateway-service/src/main/java/com/demo/stockmarket/gateway/controller/AuthenticationController.java
+++ stockmarket-gateway-service/src/main/java/com/demo/stockmarket/gateway/controller/AuthenticationController.java
@@ -5,12 +5,12 @@ import javax.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.demo.stockmarket.entity.RestResponse;
 import com.demo.stockmarket.entity.User;
 import com.demo.stockmarket.gateway.entity.LoginResponse;
 import com.demo.stockmarket.gateway.service.AuthenticationService;
@@ -27,19 +27,19 @@ public class AuthenticationController {
 	private TokenService tokenService;
 	
 	@RequestMapping(value = "/api/login", method = RequestMethod.POST, consumes = MediaType.APPLICATION_JSON_VALUE)
-    public ResponseEntity<?> login(@Valid @RequestBody User loginUser) {
+    public RestResponse<?> login(@Valid @RequestBody User loginUser) {
         User user = authService.getUserByEmail(loginUser.getEmail());
         String token = null;
         
         if (user == null) {
-        	return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("Invalid username or password.");
+        	return RestResponse.error(HttpStatus.UNAUTHORIZED.value(), "Invalid username or password.");
         }
         
         if (user.getPassword().equals(loginUser.getPassword())) {
         	token = tokenService.createToken(user.getEmail(), user.getRole());
         	
         } else {
-        	return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("Invalid username or password.");
+        	return RestResponse.error(HttpStatus.UNAUTHORIZED.value(), "Invalid username or password.");
         }
         
         LoginResponse loginResponse = new LoginResponse();
@@ -48,6 +48,6 @@ public class AuthenticationController {
         loginResponse.setEmail(user.getEmail());
         loginResponse.setRole(user.getRole());
         
-        return ResponseEntity.ok(loginResponse);
+        return RestResponse.ok(loginResponse, "Login successfully.");
     }
 }
