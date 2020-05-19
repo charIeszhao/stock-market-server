9b1efeb14d741710730830578d1c0ca278ee95a1
Charles Zhao <charles.zhao@cn.ibm.com>
2020-05-20 01:06:18 +0800

    update common entities to improve response json structure
    Former-commit-id: dc4810ff55db640f8dcd583cdb04d3146d44f147

--- stockmarket-common/src/main/java/com/demo/stockmarket/entity/RestResponse.java
+++ stockmarket-common/src/main/java/com/demo/stockmarket/entity/RestResponse.java
@@ -8,36 +8,76 @@ import lombok.NoArgsConstructor;
 public class RestResponse<T> {
 
     private static final int OK_CODE = 200;
+    private static final int CREATED_CODE = 201;
+    private static final int NO_CONTENT_CODE = 204;
 
     private T data;
     private String message;
     private int code;
+    private Long count;
+    private boolean success;
 
-
-    public RestResponse(int code, String message, T data) {
+    public RestResponse(int code, String message, T data, boolean success) {
         this.data = data;
         this.message = message;
         this.code = code;
+        this.success = success;
+    }
+
+    public RestResponse(int code, String message, T data, Long count, boolean success) {
+        this.data = data;
+        this.message = message;
+        this.code = code;
+        this.count = count;
+        this.success = success;
     }
 
     public static <T> RestResponse<T> ok() {
-        return new RestResponse<T>(OK_CODE, null, null);
+        return new RestResponse<T>(OK_CODE, "", null, true);
     }
 
     public static <T> RestResponse<T> ok(T data) {
-        return new RestResponse<T>(OK_CODE, null, data);
+        return new RestResponse<T>(OK_CODE, "", data, true);
     }
 
     public static <T> RestResponse<T> ok(T data, String message) {
-        return new RestResponse<T>(OK_CODE, message, data);
+        return new RestResponse<T>(OK_CODE, message, data, true);
+    }
+    
+    public static <T> RestResponse<T> ok(T data, long total, String message) {
+        return new RestResponse<T>(OK_CODE, message, data, total, true);
+    }
+    
+    public static <T> RestResponse<T> created(T data) {
+        return new RestResponse<T>(CREATED_CODE, "", data, true);
+    }
+
+    public static <T> RestResponse<T> created(T data, String message) {
+        return new RestResponse<T>(CREATED_CODE, message, data, true);
+    }
+    
+    public static <T> RestResponse<T> updated(T data) {
+        return new RestResponse<T>(OK_CODE, "", data, true);
+    }
+
+    public static <T> RestResponse<T> updated(T data, String message) {
+        return new RestResponse<T>(OK_CODE, message, data, true);
+    }
+    
+    public static <T> RestResponse<T> deleted(T data) {
+        return new RestResponse<T>(NO_CONTENT_CODE, "", data, true);
+    }
+
+    public static <T> RestResponse<T> deleted(T data, String message) {
+        return new RestResponse<T>(NO_CONTENT_CODE, message, data, true);
     }
 
     public static <T> RestResponse<T> error(int code, String message) {
-        return new RestResponse<T>(code, message, null);
+        return new RestResponse<T>(code, message, null, false);
     }
 
     public static <T> RestResponse<T> error(int code, String message, T data) {
-        return new RestResponse<T>(code, message, data);
+        return new RestResponse<T>(code, message, data, false);
     }
 
 }
